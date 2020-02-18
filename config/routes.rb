6e868db715fbb69c5dfae47b1424b073e1e12de4
juancarlosasensio <jasensio@ufm.edu>
2020-02-18 15:21:28 +0000

    Implement all CRUD methods in Rails using ActiveRecord

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   #read all tasks
   get 'tasks', to: 'tasks#index'
   #read one task
+  get 'tasks/new', to: 'tasks#new'
   get 'tasks/:id', to: 'tasks#show', as: 'task'
+  #create a task
+  post 'tasks', to: 'tasks#create'
+  #edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  #destroy
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
