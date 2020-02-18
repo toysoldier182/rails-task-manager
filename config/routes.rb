ee29225ffe4a386ba20447c1058adf009faa6213
juancarlosasensio <jasensio@ufm.edu>
2020-02-18 12:20:14 +0000

    Tasks show action and view done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #read all tasks
   get 'tasks', to: 'tasks#index'
+  #read one task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
